ab28e36b89c5f6a5a420a9276c6623f9f72c262d
carlosaegar <carlos.saegar@educa.jcyl.es>
2026-02-18 11:50:31 +0100

    Update Piloto.java

--- src/Piloto.java
+++ src/Piloto.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class Piloto {
+    private String nombre;
+    private String escuderia;
+    private int puntos;
+
+    public Piloto(String nombre, String escuderia, int puntos) {
+        this.nombre = nombre;
+        this.escuderia = escuderia;
+        this.puntos = puntos;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEscuderia() {
+        return escuderia;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEscuderia(String escuderia) {
+        this.escuderia = escuderia;
+    }
+
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
+    }
 }
