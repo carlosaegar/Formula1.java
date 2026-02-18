10b0af2a6dc39daf3b21bbf5b287a6f554e9db05
carlosaegar <carlos.saegar@educa.jcyl.es>
2026-02-18 11:50:20 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,99 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+
+public class Main{
+
+
+    public static void main(String[] args) {
+        ArrayList<Piloto> pilotos = new ArrayList<>();
+        cargarDatos(pilotos);
+        mostrarPilotos(pilotos);
+        añadirPiotos(pilotos);
+        mostrarPilotos(pilotos);
+       // buscarPiloto(pilotos);
+        escribirFichero(pilotos);
+    }
+
+
+        public static void cargarDatos(ArrayList < Piloto > pilotos) {
+            try (BufferedReader reader = new BufferedReader(new FileReader("pilotos_f1.txt"))) {
+
+                String linea;
+                while ((linea = reader.readLine()) != null) {
+                    String[] campos = linea.split(";");
+                    Piloto piloto = new Piloto(campos[0], campos[1], Integer.parseInt(campos[2]));
+                    pilotos.add(piloto);
+                }
+            } catch (IOException e) {
+                System.out.println("Ocurrió un error al leer el archivo; " + e.getMessage());
+            }
+        }
+
+
+    public static void mostrarPilotos(ArrayList<Piloto> pilotos) {
+        for (Piloto e : pilotos) {
+            System.out.println("Nombre " + e.getNombre());
+            System.out.println("Escuderia " + e.getEscuderia());
+            System.out.println("Puntos " + e.getPuntos());
+            System.out.println(" ");
+        }
+    }
+
+    public static void añadirPiotos(ArrayList<Piloto> pilotos) {
+        String nombre = "Sainz";
+        String escuderia = "Ferrari";
+        int puntos = 203;
+        boolean repetido = false;
+        //comprobamos si existen los pilotos en el ArrayList
+        if (existePiloto(pilotos, nombre)) {
+            System.out.println("El piloto ya existe");
+        }else {
+            pilotos.add(new Piloto(nombre,escuderia,puntos));
+        }
+
+    }
+
+    public static void buscarPiloto(ArrayList<Piloto> pilotos){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce el nombre del piloto a buscar: ");
+        String nombre = sc.nextLine();
+
+        if (existePiloto(pilotos,nombre)){
+            System.out.println("El piloto existe");
+
+        }else {
+            System.out.println("EL piloto no existe");
+        }
+    }
+
+    public static boolean existePiloto (ArrayList<Piloto> pilotos, String nombre){
+        boolean encontrado = false;
+        for (Piloto p : pilotos ){
+            if (p.getNombre().equals(nombre)){
+                encontrado = true;
+                break;
+            }
+        }
+        return encontrado;
+    }
+
+    public static void escribirFichero(ArrayList<Piloto> pilotos) {
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter("pilotos_f1.txt"))){
+            for (Piloto p :pilotos){
+                writer.write(p.getNombre()+";"+p.getEscuderia()+";"+p.getPuntos());
+                writer.newLine();
+            }
+            System.out.println("Lineas escritas en el archivo");
+        }catch (IOException e){
+            System.out.println("Ocurrio un problema al modificar " + e.getMessage());
+        }
+    }
 }
